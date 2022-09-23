982cd6b903febdd66160938fff90be9f30a998ac
jeffreyderas <jefreyjb10@gmail.com>
2022-09-23 16:38:31 -0300

    1.3

---
+++ src/CondiçãoTernaria.java
@@ -0,0 +1,11 @@
+package src;
+
+public class CondiçãoTernaria {
+    public static void main(String[] args) {
+        int nota = 5;
+            String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+
+        System.out.println(resultado);
+    }
+}
---
+++ src/SwitchCase.java
@@ -0,0 +1,32 @@
+package src;
+
+/*Sistema de medida */
+public class SwitchCase {
+    public static void main(String[] args) {
+        String sigla = "";
+
+           switch (sigla) {
+            case "P":{
+                System.out.println("Pequeno");
+                break;
+            }
+
+            case "M":{
+                System.out.println("Medio");
+                break;
+            }
+
+            case "G":{
+                System.out.println("Grande");
+                break;
+            }
+
+            default:
+            System.out.println("Indefinido");
+            break;
+
+        }}}
+           
+    
+    
+
