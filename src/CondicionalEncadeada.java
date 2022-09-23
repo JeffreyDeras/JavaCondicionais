b1f2825ee3b84ef64a0dd570fde26515d8be2929
jeffreyderas <jefreyjb10@gmail.com>
2022-09-23 14:17:53 -0300

    1.2

---
+++ src/CondicionalEncadeada.java
@@ -0,0 +1,17 @@
+package src;
+/*condicional encadeada */
+public class CondicionalEncadeada {
+    public static void main(String[] args) {
+        
+        int nota = 9;
+
+        if(nota >= 7)
+        System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7)
+        System.out.println("Prova de recuperação");
+
+        else 
+        System.out.println("Reprovado");
+    }
+}
